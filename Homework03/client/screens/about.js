db05a539c80a7c2bcdd8efc97700e5ed62846574
유건주 <81522323+bakgom123@users.noreply.github.com>
2022-11-05 01:03:19 -0400

    Delete Homework03 directory

--- Homework03/client/screens/about.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-export default function AboutScreen({ navigation }) {
-    return (
-        <Text>This application acesses information on Monopoly players.</Text>
-    );
-};
