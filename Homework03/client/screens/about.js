1e4e47c24c122429b975ccf79701287d0f4016f4
rjswn97 <gunjudavid@naver.com>
2022-11-05 00:43:52 -0400

    hw3 submission

---
+++ Homework03/client/screens/about.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function AboutScreen({ navigation }) {
+    return (
+        <Text>This application acesses information on Monopoly players.</Text>
+    );
+};
